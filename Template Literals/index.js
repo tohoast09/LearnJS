a2dddd019de1bbf1cba2fb836fe37a85d54ae77d
To Hoa <tohoast09@gmail.com>
2023-11-08 15:08:28 +0700

    template literals

---
+++ Template Literals/index.js
@@ -0,0 +1,13 @@
+
+//Template string
+const name = 'To Hoa';
+const action = 'learn';
+
+const noi = `${name} đang ${action}`;
+console.log(noi);
+//Viết nhiều dòng
+
+const lines = 'Line 1\n'
+    + 'Line 2\n'
+    + 'Line 3\n'
+console.log(lines);
